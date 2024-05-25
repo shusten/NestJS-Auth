6a5c1e808090d77264ff73e1fed50adcfdcc9c85
Luis Villas-Boas <shusten@hotmail.com>
2024-05-25 20:19:40 -0300

    add users post

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { UserDto } from './user.dto';
+import {v4 as uuid} from 'uuid';
+import { hashSync as bcryptHashSync } from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private readonly users: UserDto[] = []
+
+
+    create(newUser: UserDto) {
+        newUser.id = uuid();
+        newUser.password = bcryptHashSync(newUser.password, 10);
+        this.users.push(newUser);
+        console.log(this.users);
+    }
+}
