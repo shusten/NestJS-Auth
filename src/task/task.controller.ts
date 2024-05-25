b3dd0fb15ba925cce0a6bea503168d8da9e5faeb
Luis Villas-Boas <shusten@hotmail.com>
2024-05-25 00:46:16 -0300

    Add put task

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { TaskDto } from './task.dto';
 import { TaskService } from './task.service';
 
@@ -15,5 +15,10 @@ export class TaskController {
     @Get('/:id')
     findById(@Param('id') id:string): TaskDto {
         return this.taskservice.findById(id);
+    }
+
+    @Put()
+    update(@Body() task: TaskDto) {
+        this.taskservice.update(task);
     }
 }
