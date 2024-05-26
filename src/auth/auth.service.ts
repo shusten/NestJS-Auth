1ae49dfa37df29afdc1c6827f21522df7c13c2a4
Luis Villas-Boas <shusten@hotmail.com>
2024-05-25 23:21:17 -0300

    :construction: Implementing signIn method in auth service.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+import { AuthResponseDto } from './auth.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly usersService: UsersService,
+        private readonly jwtService: JwtService
+    ) {}
+
+    signIn(username: string, password: string): AuthResponseDto {
+        /* const foundUser = this.usersService.findByUserName(username); */
+        return 
+    }
+}
